eb206abf438b9e4d1e90931c837ac17bbe52186e
Joseph Zullo <jzullo@mddx.com>
2020-10-07 18:02:27 -0400

    refactored to loop even after success

---
+++ services/console_printer.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+def print_out_of_stock():
+    print("Product out of stock: ", datetime.datetime.now())
+
+
+def print_in_stock():
+    print("Product in stock!")
--- services/scraper.py
+++ services/scraper.py
@@ -1,10 +1,7 @@
 import requests
-import time
-import datetime
 
 url_for_rtx_3080 = 'https://www.newegg.com/asus-geforce-rtx-3080-tuf-rtx3080-10g-gaming/p/N82E16814126453?Item=N82E16814126453'
 out_of_stock_warning = """<div class="product-inventory"><strong><i class="fas fa-exclamation-triangle"></i> OUT OF STOCK.</strong></div>"""
-check_frequency = 10
 
 
 def get_html_from_newegg():
@@ -18,9 +15,4 @@ def check_if_product_out_of_stock(html):
 
 def scrape():
     html = get_html_from_newegg()
-    product_out_of_stock = check_if_product_out_of_stock(html)
-    if product_out_of_stock:
-        print("Product out of stock: ", datetime.datetime.now())
-        time.sleep(check_frequency)
-        return True
-    return False
+    return check_if_product_out_of_stock(html)
